4c945d4865ba4c6a37ae02ba2980edaeac1e846c
malthadius <malthadius@gmail.com>
2020-07-04 18:38:04 +0100

    Added side and top navigation bar components

---
+++ frontend/src/components/SideNav.vue
@@ -0,0 +1,129 @@
+<template>
+    <div id="side_nav">
+        <div id="top_logo"></div>
+        <div id="top_logo_bottom">
+            <router-link id="headquarters" to="headquarters">Headquarters</router-link>
+        </div>
+        <div class="side_nav_main" id="side_nav_empire_management">
+            <router-link to="council">Council</router-link>
+            <router-link to="units">Units</router-link>
+            <router-link to="market">Market</router-link>
+            <router-link to="planets">Planets</router-link>
+            <router-link id="empire_section" to="empire">Empire</router-link>
+            <router-link class="sublink" to="empire_forum">- Forum</router-link>
+            <router-link class="sublink" to="send_aid">- Send aid</router-link>
+            <router-link class="sublink" to="receive_aid">- Receive aid</router-link>
+            <router-link class="sublink" to="News">- News</router-link>
+            <router-link class="sublink" to="Relations">- Relations</router-link>
+            <router-link to="fleets">Fleets</router-link>
+            <router-link id="galaxy_section" to="galaxy_map">Galaxy map</router-link>
+            <router-link class="sublink" to="full_map">- Full map</router-link>
+            <router-link class="sublink" to="map_generation">- Map Gen</router-link>
+            <router-link class="sublink" to="scouting">- Scouting</router-link>
+            <router-link to="research">Research</router-link>
+            <router-link to="operations">Operations</router-link>
+        </div>
+        <div id="side_nav_divider"></div>
+        <div class="side_nav_main" id="side_nav_bottom_management">
+            <router-link to="messages">Messages</router-link>
+            <router-link to="faction_rankings">Faction rankings</router-link>
+            <router-link to="empire_rankings">Empire rankings</router-link>
+            <router-link to="forums">Forums</router-link>
+            <router-link to="account">Account</router-link>
+            <router-link id="logout" to="logout">Logout</router-link>
+            <form action="search" method="POST">
+                <input type="text" name="search" size="8" value="">
+                <input type="submit" size="2" value="OK">
+            </form>
+            <router-link id="discord" to="Discord">Discord</router-link>
+            <router-link to="Guide">Guide</router-link>
+            <router-link to="Facebook">Facebook</router-link>
+        </div>
+        <div id="side_nav_bottom"></div>
+        <div id="side_nav_bottom_logo"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SideNav"
+    }
+</script>
+
+<style scoped>
+
+    #side_nav{
+        margin-top: 15px;
+        margin-right: 20px;
+        display: flex;
+        flex-direction: column;
+        width: 155px;
+        height: 100%;
+        font-weight: bold;
+        font-size: 13px;
+        text-align: left;
+        font-family: Tahoma,serif;
+        color: #C0D0D8;
+    }
+
+    #top_logo{
+        width: 150px;
+        height: 40px;
+        background-image: url('../assets/side_nav_images/i18.jpg');
+    }
+    #top_logo_bottom{
+        width: 150px;
+        height: 20px;
+        background-image: url('../assets/side_nav_images/i23.jpg');
+
+    }
+    #headquarters{
+        vertical-align: sub;
+    }
+    #logout{
+        padding-bottom: 16px;
+    }
+    #discord{
+        padding-top: 10px;
+    }
+    .sublink{
+        padding-left: 8px;
+    }
+    .side_nav_main{
+        display: flex;
+        flex-direction: column;
+        width: 150px;
+        background-image: url('../assets/side_nav_images/i36.jpg');
+    }
+    #empire_section{
+        display: flex;
+        flex-direction: column;
+    }
+    #galaxy_section{
+        display: flex;
+        flex-direction: column;
+
+    }
+    #side_nav_empire_management{
+        height: 272px;
+    }
+    #side_nav_bottom_management{
+        height: 193px
+    }
+    #side_nav_divider{
+        width: 150px;
+        height: 15px;
+        padding-bottom: 3px;
+        background-image: url('../assets/side_nav_images/i53.jpg');
+    }
+    #side_nav_bottom{
+        width: 150px;
+        height: 20px;
+        background-image: url('../assets/side_nav_images/i55.jpg');
+    }
+    #side_nav_bottom_logo{
+        width: 150px;
+        height: 75px;
+        background-image: url('../assets/side_nav_images/i56.jpg');
+    }
+</style>
---
+++ frontend/src/components/TopNav.vue
@@ -0,0 +1,185 @@
+<template>
+    <div class="nav_text" id="top_nav">
+        <div id="left_bookend"></div>
+        <div id="center">
+            <div id="top">
+                <div id="top_left_bookend"></div>
+                <div id="top_center">
+                    <div class="top_nav_text" id="networth">Networth: {{networth}}</div>
+                    <div class="top_nav_text" id="next_tick">Next Tick: {{next_tick}}</div>
+                    <div class="top_nav_text" id="population">Population: {{population}}</div>
+                </div>
+                <div id="top_right_bookend"></div>
+            </div>
+            <div id="bottom_row">
+                <router-link id=icons to="headquarters">
+                    <div id="mail_icon"></div>
+                    <div id="build_icon"></div>
+                    <div id="aid_icon"></div>
+                    <div id="fleet_icon"></div>
+                    <div id="slant_img"></div>
+                </router-link>
+                <div id="bottom_center">
+                    <div id="left_resources">
+                        <div class="resource_text" id="Energy">Energy: {{energy}}</div>
+                        <div class="resource_text" id="Mineral">Mineral: {{mineral}}</div>
+                    </div>
+                    <div id="right_resources">
+                        <div class="resource_text" id="Crystal">Crystal: {{crystal}}</div>
+                        <div class="resource_text" id="Ectrolium">Ectrolium: {{ectrolium}}</div>
+                    </div>
+                </div>
+                <div id="bottom_right_bookend"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TopNav",
+        data () {
+            return {
+                networth: 7290193,
+                population: 2013809312,
+                next_tick: "10 seconds",
+                energy: 712312432,
+                mineral: 3411212,
+                crystal: 21301,
+                ectrolium: 1231314,
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    #top_nav{
+        display: flex;
+        flex-direction: row;
+    }
+    #left_bookend {
+        width: 35px;
+        height: 63px;
+        background-image: url('../assets/top_nav_images/i02.jpg');
+    }
+    #center {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+
+    /* Top row of nav bar section*/
+    #top {
+        display: flex;
+        flex-direction: row;
+    }
+    #top_left_bookend{
+        width: 41px;
+        height: 21px;
+        background-image: url('../assets/top_nav_images/i03.jpg');
+    }
+    #top_center{
+        display: flex;
+        flex-direction: row;
+        width: 100%;
+        height: 21px;
+        justify-content: center;
+        align-items: center; /* align vertical */
+        background-image: url('../assets/top_nav_images/i05.jpg');
+    }
+    #networth{
+        float: left;
+        width: 33%;
+    }
+    #next_tick{
+        width: 33%;
+    }
+    #population{
+        float: right;
+        width: 33%;;
+    }
+    #top_right_bookend {
+        width: 78px;
+        height: 21px;
+        background-image: url('../assets/top_nav_images/i07.jpg');
+    }
+
+    /* Bottom row of nav bar section*/
+    #bottom_row{
+        display: flex;
+        flex-direction: row;
+        width: 100%;
+    }
+    #icons{
+        display: flex;
+        flex-direction: row;
+        height: 42px;
+        width: auto;
+    }
+    #mail_icon{
+        height: 42px;
+        width: 41px;
+        background-image: url('../assets/top_nav_images/i09.jpg');
+    }
+    #build_icon{
+        height: 42px;
+        width: 40px;
+        background-image: url('../assets/top_nav_images/i10.jpg');
+
+    }
+    #aid_icon{
+        height: 42px;
+        width: 39px;
+        background-image: url('../assets/top_nav_images/i11.jpg');
+    }
+    #fleet_icon{
+
+        height: 42px;
+        width: 39px;
+        background-image: url('../assets/top_nav_images/i12.jpg');
+    }
+    #slant_img {
+        height: 42px;
+        width: 18px;
+        background-image: url('../assets/top_nav_images/i13.jpg');
+    }
+    #bottom_center {
+        display: flex;
+        flex-direction: row;
+        width: 100%;
+        height: 42px;
+        background-image: url('../assets/top_nav_images/i15.jpg');
+    }
+    #left_resources{
+        display: flex;
+        flex-direction: column;
+        width: 50%
+    }
+    #right_resources{
+        display: flex;
+        flex-direction: column;
+        width: 50%
+    }
+    #bottom_right_bookend {
+        width: 49px;
+        height: 42px;
+        background-image: url('../assets/top_nav_images/i17.jpg');
+    }
+    .nav_text{
+      font-family: verdana,geneva,arial,helvetica,sans-serif;
+      color: #C0D0D8;
+      font-weight: bold;
+    }
+    .top_nav_text {
+      font-size: 11px;
+      text-align: center;
+    }
+    .resource_text{
+        font-size: 11px;
+        text-align: center;
+        justify-content: center;
+        padding: 2px;
+        align-items: center; /* align vertical */
+    }
+</style>
